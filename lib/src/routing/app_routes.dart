a23fc1d67b74aed60fbca6cc87cf80bbe7af9617
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-10 19:22:14 +0300

    add app routes constants

---
+++ lib/src/routing/app_routes.dart
@@ -0,0 +1,15 @@
+abstract final class AppRoutes {
+  AppRoutes._();
+
+  static const String splash = '/splash';
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String forgotPassword = '/forget-password';
+  static const String verifyCode = '/verify-code';
+  static const String newPassword = '/new-password';
+  static const String favourites = '/favourites';
+  static const String courses = '/courses';
+  static const String profile = '/profile';
+  static const String settings = '/profile/settings';
+}
