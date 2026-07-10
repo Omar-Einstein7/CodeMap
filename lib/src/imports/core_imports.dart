b6fe0a4be9f184b3dff1b6e7d3be3378b3bc8534
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-10 19:22:05 +0300

    add core imports barrel

---
+++ lib/src/imports/core_imports.dart
@@ -0,0 +1,17 @@
+export 'package:flutter/material.dart';
+export 'package:flutter/cupertino.dart' hide RefreshCallback;
+export 'package:flutter/foundation.dart' hide describeIdentity, shortHash;
+export 'package:flutter/services.dart';
+
+export '../config/api_urls.dart';
+export '../config/app_config.dart';
+export '../config/generic_data_cubit.dart';
+export '../config/generic_data_state.dart';
+export '../routing/app_router.dart';
+export '../routing/app_routes.dart';
+export '../routing/global_navigator.dart';
+export '../services/services.dart';
+export '../shared/shared.dart';
+export '../extensions/extensions.dart';
+export '../utils/utils.dart';
+export '../theme/theme.dart';
