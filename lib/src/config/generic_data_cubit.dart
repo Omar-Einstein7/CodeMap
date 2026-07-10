bbbf5d98a12146f1a400903ae8f368c8f358795c
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-10 19:21:45 +0300

    add generic data cubit

---
+++ lib/src/config/generic_data_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'generic_data_state.dart';
+import '../utils/usecase.dart';
+
+class GenericDataCubit extends Cubit<GenericDataState> {
+  
+  GenericDataCubit() : super(DataLoading());
+
+
+  void getData<T>(Usecase usecase, {dynamic params}) async {
+    var returnedData = await usecase.call(params: params);
+    returnedData.fold(
+      (error){
+        emit(
+          FailureLoadData(errorMessage: error)
+        );
+      },
+      (data){
+        emit(
+          DataLoaded<T>(data: data)
+        );
+      }
+    );
+  }
+
+ 
+}
