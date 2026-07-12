5729c00e327ad37ca2f5000a506b951d45861922
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-13 02:09:55 +0300

    refactor: remove unused Dio instance from AppConfig

--- lib/src/config/app_config.dart
+++ lib/src/config/app_config.dart
@@ -1,21 +1,5 @@
-import 'package:dio/dio.dart';
-import 'api_urls.dart';
-
 class AppConfig {
   AppConfig._();
-  static late final Dio dio;
 
-  static Future<void> init() async {
-    dio = Dio(
-      BaseOptions(
-        baseUrl: APIUrls.baseURL,
-        connectTimeout: const Duration(seconds: 30),
-        receiveTimeout: const Duration(seconds: 30),
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-        },
-      ),
-    );
-  }
+  static Future<void> init() async {}
 }
