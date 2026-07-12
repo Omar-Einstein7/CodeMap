49e61ff21dd24788d3331c67ca4524e174159ccf
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-13 02:09:58 +0300

    refactor: remove dead code exports

--- lib/src/imports/core_imports.dart
+++ lib/src/imports/core_imports.dart
@@ -5,11 +5,8 @@ export 'package:flutter/services.dart';
 
 export '../config/api_urls.dart';
 export '../config/app_config.dart';
-export '../config/generic_data_cubit.dart';
-export '../config/generic_data_state.dart';
 export '../routing/app_router.dart';
 export '../routing/app_routes.dart';
-export '../routing/global_navigator.dart';
 export '../services/services.dart';
 export '../shared/shared.dart';
 export '../extensions/extensions.dart';
