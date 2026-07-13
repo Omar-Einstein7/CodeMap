1cb9e209f7da2b3690df7346b8b19e61ad72a37f
Omar_Ahmed20 <109938759+Omar-Einstein7@users.noreply.github.com>
2026-07-13 16:58:10 +0300

    feat(repo): add getFeaturedCourses and getCategories to interface

--- lib/src/features/course/domain/repositories/course_repository.dart
+++ lib/src/features/course/domain/repositories/course_repository.dart
@@ -4,6 +4,8 @@ abstract class CourseRepository {
   Future<List<Course>> getCoursesByCategory(CourseCategory category);
   Future<List<Course>> searchCourses(String query, CourseCategory category);
   Future<List<Course>> globalSearch(String query);
+  Future<List<String>> getCategories();
+  Future<List<Course>> getFeaturedCourses();
 
   /// Fetches the full course details including Sections and Lessons.
   /// This is separate because the list view doesn't need the full hierarchy.
